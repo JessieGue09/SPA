b46d80cadb75017fd418193852b8d6d9130a384c
JessieGue09 <jessy.v.hdez0028@gmail.com>
2020-06-10 07:03:07 -0600

    Proyecto SPA concluido.

---
+++ src/components/app.vue
@@ -0,0 +1,33 @@
+<template>
+<div>
+  <p>Hello Jessie</p>
+
+  <VueButton text="Primer boton"></VueButton>
+  <VueButton text="Hola"></VueButton>
+  <VueButton text="Que tal?"></VueButton>
+  <VueButton text="Que onda?"></VueButton>
+
+ <ul>
+      <li>Elemento 1</li>
+      <li>Elemento 2</li>
+  </ul>
+
+</div>
+</template>
+
+<script>
+import './../css/style.css';
+import VueButton from './VueButton.vue';
+
+export default {
+    components: {
+        VueButton
+    }
+}
+</script>
+
+<style>
+    p{
+        color: red;;
+    }
+</style>
